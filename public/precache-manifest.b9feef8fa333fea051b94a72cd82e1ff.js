2f6b54cb1a2b1cf1464fbdb682563ad5bf642b0e
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2019-08-23 13:05:23 +0200

    before final deploy

---
+++ public/precache-manifest.b9feef8fa333fea051b94a72cd82e1ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a662ce7c9f9f0f1e5e89ddb2afc7d9d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2e402056ab6cb3764bc",
+    "url": "/static/css/3.c8c8731b.chunk.css"
+  },
+  {
+    "revision": "c88430f76b645cb70114",
+    "url": "/static/css/main.fc09254d.chunk.css"
+  },
+  {
+    "revision": "e2ec3b2ab959ebcafe9a",
+    "url": "/static/js/2.30baa994.chunk.js"
+  },
+  {
+    "revision": "f2e402056ab6cb3764bc",
+    "url": "/static/js/3.aae23c60.chunk.js"
+  },
+  {
+    "revision": "c88430f76b645cb70114",
+    "url": "/static/js/main.c06e78bb.chunk.js"
+  },
+  {
+    "revision": "89f2d74814ac1892fb07",
+    "url": "/static/js/runtime~main.48786664.js"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39fe862bb349fb8ef9e3290b1f64d2c0.js"
+  "/precache-manifest.b9feef8fa333fea051b94a72cd82e1ff.js"
 );
 
 self.addEventListener('message', (event) => {
