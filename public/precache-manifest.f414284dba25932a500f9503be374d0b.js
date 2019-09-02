8c3134e9696f3bca4628614f995445381839afb5
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2019-09-02 19:19:00 +0200

    added responsiveness to creation pages

--- public/precache-manifest.f414284dba25932a500f9503be374d0b.js
+++ public/precache-manifest.f414284dba25932a500f9503be374d0b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d7ab14b039aa612ef6de17265cd9c994",
+    "revision": "cf05778358bde489a2e95c70261cdb5d",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/3.31a97f98.chunk.css"
   },
   {
-    "revision": "60bff36fcd5c8582dd96",
-    "url": "/static/css/main.e9d03614.chunk.css"
+    "revision": "ea9e9cefc34a6a5bad69",
+    "url": "/static/css/main.76ce8e8a.chunk.css"
   },
   {
     "revision": "6f53abfde95ec946667a",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/3.e0f68eff.chunk.js"
   },
   {
-    "revision": "60bff36fcd5c8582dd96",
-    "url": "/static/js/main.482b7367.chunk.js"
+    "revision": "ea9e9cefc34a6a5bad69",
+    "url": "/static/js/main.34b489da.chunk.js"
   },
   {
     "revision": "1769610fbc8588cf9051",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.955b698853d77f307a13a1f864cada05.js"
+  "/precache-manifest.f414284dba25932a500f9503be374d0b.js"
 );
 
 self.addEventListener('message', (event) => {
